75ef36602737fdeca6112bfeea6a9a378c0ad8bc
cristianreynaga <cristianreynaga@Cristians-MacBook-Pro.local>
2014-05-17 15:51:33 -0300

    update arduino

--- arduino/cestoSensado_1b/cestoSensado_1b.ino
+++ arduino/cestoSensado_1b/cestoSensado_1b.ino
@@ -10,7 +10,7 @@
 
 #define TRIGGER_PIN  11  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     12  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define MAX_DISTANCE 500 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
